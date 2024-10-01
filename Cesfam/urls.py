9fbad6f340e5446aafda18d707d74fbddd402d29
LucasCarcamoA <lucascarcamoalvarez@gmail.com>
2024-10-01 13:35:58 -0300

    Template "sobre nosotros" agregado y diseñado

--- Cesfam/urls.py
+++ Cesfam/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Cesfampage.views import index
+from Cesfampage.views import index, sobre_nosotros
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('sobrenosotros/', sobre_nosotros)
 ]
