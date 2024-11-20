8275f33e792ad2b6a411571545742f8566df2c75
LucasCarcamoA <lucascarcamoalvarez@gmail.com>
2024-11-20 02:55:14 -0300

    AllowedHost

--- Cesfam/settings.py
+++ Cesfam/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7q9%ei)mjgd(()i!pqs!9p$ir3(wjff_et&l&r43z%hz_gtiy*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cesfamangachilla-atamh6fac8dneuh2.brazilsouth-01.azurewebsites.net']
 
 
 # Application definition
